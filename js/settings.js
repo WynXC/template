0c95fca516eccd0e52c65b791a0fd07eb6ce8223
Wync <Tootie-@live.com.sg>
2022-06-13 16:04:40 +0800

    .

--- js/settings.js
+++ js/settings.js
@@ -18,11 +18,11 @@ const collectionInfo = {
     }
 }
 const mintInfo = {
-    price: 1,
+    price: 0,
     totalSupply: 100,
     toFixed: 0,
     minUnits: 1,
-    maxUnits: 3,
+    maxUnits: 1,
     askMintLoop: true,
 }
 
