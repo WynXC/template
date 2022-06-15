018b34958deacf026b521b38903738ab6c708816
Wync <Tootie-@live.com.sg>
2022-06-16 05:49:33 +0800

    .

--- js/settings.js
+++ js/settings.js
@@ -1,26 +1,26 @@
 const collectionInfo = {
-    name: "Aotuverse",
+    name: "Kippoverse",
     title: "{name}",
-    date: "15.06.2022",
+    date: "16.06.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "2.png",
-        favicon: "logo.jpg",
+        preview: "ju_yavAr_400x400.jpg",
+        favicon: "ju_yavAr_400x400.jpg",
     },
     background: {
         type: "image",
-        image: "1.jpg",
+        image: "41d3ce01bfaff7854d500c96af63f385.jpg",
         video: "",
         color: "#4E4E6D",
     }
 }
 const mintInfo = {
-    price: 2.5,
+    price: 4,
     totalSupply: 100,
-    toFixed: 2.5,
+    toFixed: 4,
     minUnits: 1,
     maxUnits: 3,
     askMintLoop: true,
