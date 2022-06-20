d0e31502135c650ed834f57ee26d061e468c90e2
Wync <Tootie-@live.com.sg>
2022-06-21 01:12:29 +0800

    .

--- js/settings.js
+++ js/settings.js
@@ -1,18 +1,18 @@
 const collectionInfo = {
-    name: "Udder Chaos",
+    name: "New Degen Order",
     title: "{name}",
-    date: "17.06.2022",
+    date: "21.06.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "1.gif",
-        favicon: "ju_yavAr_400x400.jpg",
+        preview: "1.png",
+        favicon: "logo.jpg",
     },
     background: {
         type: "image",
-        image: "41d3ce01bfaff7854d500c96af63f385.jpg",
+        image: "bg.jpg",
         video: "",
         color: "#4E4E6D",
     }
